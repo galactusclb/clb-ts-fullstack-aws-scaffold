81e6b9131c05278addd980ed51712bbcc2f14270
galactus-clb <chanakabalasuriya123@gmail.com>
2026-06-09 18:42:35 +0530

    chore(cicd): fix incorrect path of apps in order to run husky actions

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 export default {
-    'api/**/*.{js,ts}': [
+    'apps/api/**/*.{js,ts}': [
         () => 'pnpm --filter api lint --fix',
         'prettier --write',
     ],
-    'web/**/*.{js,ts,tsx}': [
+    'apps/web/**/*.{js,ts,tsx}': [
         () => 'pnpm --filter web lint --fix',
         'prettier --write',
     ],
